d0e28d2c73afc43c86b50ea9cfc9f6256ce11f27
Andrew Grant <andrewgrant243@gmail.com>
2019-12-16 08:40:58 -0500

    Add avro factory classes

---
+++ src/main/java/andrewgrant/friendsdrinks/user/UserAvro.java
@@ -0,0 +1,115 @@
+package andrewgrant.friendsdrinks.user;
+
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import andrewgrant.friendsdrinks.user.avro.*;
+
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+
+
+/**
+ * Class for building User serializer and deserializer.
+ */
+public class UserAvro {
+
+    private Properties envProps;
+    private SchemaRegistryClient registryClient;
+
+    public UserAvro(Properties envProps, SchemaRegistryClient registryClient) {
+        this.envProps = envProps;
+        this.registryClient = registryClient;
+    }
+
+    public Serializer<UserEvent> userEventSerializer() {
+        SpecificAvroSerde<UserEvent> serde = new SpecificAvroSerde<>(registryClient);
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, false);
+        return serde.serializer();
+    }
+
+    public SpecificAvroSerde<UserEvent> userEventSerde() {
+        SpecificAvroSerde<UserEvent> serde = new SpecificAvroSerde<>(registryClient);
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, false);
+        return serde;
+    }
+
+    public Serializer<UserId> userIdSerializer() {
+        SpecificAvroSerde<UserId> serde = new SpecificAvroSerde<>(registryClient);
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, true);
+        return serde.serializer();
+    }
+
+    public SpecificAvroSerde<UserId> userIdSerde() {
+        SpecificAvroSerde<UserId> serde = new SpecificAvroSerde<>(registryClient);
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, true);
+        return serde;
+    }
+
+    public SpecificAvroSerde<CreateUserRequest> createUserRequestSerde() {
+        SpecificAvroSerde<CreateUserRequest> serde = new SpecificAvroSerde<>(registryClient);
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, false);
+        return serde;
+    }
+
+    public SpecificAvroSerde<DeleteUserRequest> deleteUserRequestSerde() {
+        SpecificAvroSerde<DeleteUserRequest> serde = new SpecificAvroSerde<>(registryClient);
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, false);
+        return serde;
+    }
+
+    public SpecificAvroSerde<DeleteUserResponse> deleteUserResponseSerde() {
+        SpecificAvroSerde<DeleteUserResponse> serde = new SpecificAvroSerde<>(registryClient);
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, false);
+        return serde;
+    }
+
+    public Deserializer<UserEvent> userDeserializer() {
+        SpecificAvroSerde<UserEvent> serde = new SpecificAvroSerde<>(registryClient);
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, false);
+        return serde.deserializer();
+    }
+
+    public Deserializer<UserId> userIdDeserializer() {
+        SpecificAvroSerde<UserId> serde;
+        if (registryClient == null) {
+            serde = new SpecificAvroSerde<>();
+        } else {
+            serde = new SpecificAvroSerde<>(registryClient);
+        }
+        Map<String, String> config = new HashMap<>();
+        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                envProps.getProperty("schema.registry.url"));
+        serde.configure(config, true);
+        return serde.deserializer();
+    }
+}
