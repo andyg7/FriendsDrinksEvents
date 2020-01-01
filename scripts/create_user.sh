d1ffc860fa8aea0bef462a0b685b456be1350fac
Andrew Grant <andrewgrant243@gmail.com>
2020-01-01 17:43:43 -0500

    Fix bugs in helper scripts

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -3,4 +3,4 @@
 PORT=$1
 EMAIL=$2
 
-curl -d "{\"email\":\"${EMAIL}\"}" -H "Content-Type: application/json" -X POST http://localhost:"${PORT}"/v1/user
+curl -d "{\"email\":\"${EMAIL}\"}" -H "Content-Type: application/json" -X POST http://localhost:"${PORT}"/v1/users
--- scripts/get_user.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-PORT=$1
-
-curl -d '{"requestId":"2f66deeb-0928-4cbb-93c6-dcd00298579"}' -H "Content-Type: application/json" -X GET http://localhost:"${PORT}"/v1/user
