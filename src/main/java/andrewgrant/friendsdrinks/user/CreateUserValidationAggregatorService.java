b6a81c659247984a5a9c01b90a129532f6f8775b
Andrew Grant <andrewgrant243@gmail.com>
2020-01-04 16:33:04 -0500

    Make sure we dont emit multiple updates

--- src/main/java/andrewgrant/friendsdrinks/user/CreateUserValidationAggregatorService.java
+++ src/main/java/andrewgrant/friendsdrinks/user/CreateUserValidationAggregatorService.java
@@ -72,7 +72,16 @@ public class CreateUserValidationAggregatorService {
                 )
                 .toStream((key, value) -> key.key())
                 .filter(((key, value) -> value != null))
-                .filter(((key, value) -> value >= 2L));
+                .filter(((key, value) -> value >= 2L))
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                .reduce(((aggVal, newVal) -> {
+                    if (aggVal == 0) {
+                        return newVal;
+                    } else {
+                        return aggVal;
+                    }
+                }))
+                .toStream();
 
         final String userTopicName = envProps.getProperty("user.topic.name");
         KStream<UserId, UserEvent> userEvents = builder.stream(
