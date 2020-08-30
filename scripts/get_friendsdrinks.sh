3a86e240b2a336abc6a3c92a1eee8e737e4699a5
Andrew Grant <andrewgrant243@gmail.com>
2020-08-30 14:38:37 -0400

    Update get_friendsdrinks.sh script to handle getting by user ID

--- scripts/get_friendsdrinks.sh
+++ scripts/get_friendsdrinks.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 PORT=$1
+USER_ID=$2
 
-curl -X GET http://localhost:"${PORT}"/v1/friendsdrinks
+if [ -n "$USER_ID" ]; then
+  curl -X GET http://localhost:"${PORT}"/v1/friendsdrinks/"${USER_ID}"
+else
+  curl -X GET http://localhost:"${PORT}"/v1/friendsdrinks
+fi
