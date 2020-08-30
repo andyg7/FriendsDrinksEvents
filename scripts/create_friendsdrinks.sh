e665520fcc6148a13556b60e235167aba8faf14c
Andrew Grant <andrewgrant243@gmail.com>
2020-08-30 19:21:41 -0400

    Add new name param to script

--- scripts/create_friendsdrinks.sh
+++ scripts/create_friendsdrinks.sh
@@ -3,7 +3,8 @@
 PORT=$1
 ADMIN_USER_ID=$2
 USER_ID=$3
+NAME=$4
 
-curl -d "{\"adminUserId\":\"${ADMIN_USER_ID}\", \"scheduleType\": \"OnDemand\", \"userIds\": [\"${USER_ID}\"] }" \
+curl -d "{\"adminUserId\":\"${ADMIN_USER_ID}\", \"name\":\"${NAME}\", \"scheduleType\": \"OnDemand\", \"userIds\": [\"${USER_ID}\"] }" \
 -H "Content-Type: application/json"  \
 -X POST http://localhost:"${PORT}"/v1/friendsdrinks
