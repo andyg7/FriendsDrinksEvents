d6bb55c8d863c4bdd605e54cc1f233061e5f5789
Andrew Grant <andrewgrant243@gmail.com>
2020-09-23 21:09:04 -0400

    Force UpdateType to be Partial

--- src/main/java/andrewgrant/friendsdrinks/frontend/restapi/friendsdrinks/Handler.java
+++ src/main/java/andrewgrant/friendsdrinks/frontend/restapi/friendsdrinks/Handler.java
@@ -173,7 +173,7 @@ public class Handler {
         UpdateFriendsDrinksRequest updateFriendsDrinksRequest = UpdateFriendsDrinksRequest
                 .newBuilder()
                 .setFriendsDrinksId(FriendsDrinksId.newBuilder().setId(friendsDrinksId).build())
-                .setUpdateType(UpdateType.valueOf(requestBean.getUpdateType()))
+                .setUpdateType(UpdateType.Partial)
                 .setUserIds(userIds)
                 .setAdminUserId(requestBean.getAdminUserId())
                 .setScheduleType(scheduleType)
