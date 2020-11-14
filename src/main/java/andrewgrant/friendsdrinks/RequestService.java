83a406e68cc8586147d250f4d5c1ff75d3cb8581
Andrew Grant <andrewgrant243@gmail.com>
2020-11-14 17:38:55 -0500

    Force KTable to emit updates right away

--- src/main/java/andrewgrant/friendsdrinks/RequestService.java
+++ src/main/java/andrewgrant/friendsdrinks/RequestService.java
@@ -371,6 +371,7 @@ public class RequestService {
         Properties streamProps = new Properties();
         streamProps.put(StreamsConfig.APPLICATION_ID_CONFIG, envProps.getProperty("friendsdrinks-request.application.id"));
         streamProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, envProps.getProperty("bootstrap.servers"));
+        streamProps.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         return streamProps;
     }
 
