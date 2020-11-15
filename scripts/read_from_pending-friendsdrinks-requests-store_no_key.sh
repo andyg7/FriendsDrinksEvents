2aa307a8018e3a63f1b508ccb0af86bd8ddd5e48
Andrew Grant <andrewgrant243@gmail.com>
2020-11-15 18:18:23 -0500

    Add new scripts

---
+++ scripts/read_from_pending-friendsdrinks-requests-store_no_key.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker exec -it broker /usr/bin/kafka-console-consumer --topic friendsdrinks-request-application-pending-friendsdrinks-requests-store-changelog --bootstrap-server broker:9092 --from-beginning --property print.value=true --property print.key=true \
+    --property value.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+    --property print.value=true --property print.key=false \
+    --from-beginning
