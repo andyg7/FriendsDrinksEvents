e4ee66c64edcc5a0f603690a2e1621c4be3fb6eb
Andrew Grant <andrewgrant243@gmail.com>
2021-04-18 16:55:19 -0400

    Create bootstrap script for creating IAM user

---
+++ bootstrap/create_iam_user_secret.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+set -e
+
+aws iam create-user --user-name KubernetesManifestDeployerUserTest
+access_key=$(mktemp)
+echo "$access_key"
+aws iam create-access-key --user-name KubernetesManifestDeployerUserTest | jq -r '.AccessKey' > "$access_key"
+
+export AWS_ACCESS_KEY_ID=$(jq -r '.AccessKeyId' "$access_key")
+export AWS_SECRET_ACCESS_KEY=$(jq -r '.SecretAccessKey' "$access_key")
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]
+then
+      echo "\$AWS_ACCESS_KEY_ID is not set"
+      exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]
+then
+      echo "\$AWS_SECRET_ACCESS_KEY is empty"
+      exit 1
+fi
+
+compiled_secret=$(mktemp)
+cat bootstrap/awsiamusersecret.yaml | envsubst | tee compiled_secret
+sem create -f compiled_secret
