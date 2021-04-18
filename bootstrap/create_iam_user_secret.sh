8c8932c0a31d9503471909295f3861479b2d563a
Andrew Grant <andrewgrant243@gmail.com>
2021-04-18 17:03:13 -0400

    Clean up create_iam_user_secret.sh

--- bootstrap/create_iam_user_secret.sh
+++ bootstrap/create_iam_user_secret.sh
@@ -3,12 +3,17 @@
 set -e
 
 aws iam create-user --user-name KubernetesManifestDeployerUserTest
+aws iam put-user-policy --user-name KubernetesManifestDeployerUserTest --policy-name AWSCloudFormationReadOnlyAccess
 access_key=$(mktemp)
 echo "$access_key"
 aws iam create-access-key --user-name KubernetesManifestDeployerUserTest | jq -r '.AccessKey' > "$access_key"
 
+cat $access_key
+
 export AWS_ACCESS_KEY_ID=$(jq -r '.AccessKeyId' "$access_key")
 export AWS_SECRET_ACCESS_KEY=$(jq -r '.SecretAccessKey' "$access_key")
+
+rm -rf $access_key
 
 if [ -z "$AWS_ACCESS_KEY_ID" ]
 then
@@ -23,5 +28,6 @@ then
 fi
 
 compiled_secret=$(mktemp)
-cat bootstrap/awsiamusersecret.yaml | envsubst | tee compiled_secret
-sem create -f compiled_secret
+cat bootstrap/awsiamusersecret.yaml | envsubst | tee $compiled_secret
+sem create -f $compiled_secret
+rm -rf $compiled_secret
