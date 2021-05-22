c604c0bfc62c101f98d37cfaf5951f293a880510
Andrew Grant <andrewgrant243@gmail.com>
2021-05-21 20:10:25 -0400

    Deploy streams app deployment

--- kubernetes/streamsapp/apply.sh
+++ kubernetes/streamsapp/apply.sh
@@ -3,12 +3,11 @@
 set -eu
 
 export manifest=$1
-export deployment=$2
-export replicas=$3
-export img=$4
+export img=$2
 
 apply_tmp=$(mktemp)
 cat $manifest | envsubst | tee $apply_tmp
+cat $apply_tmp
 kubectl apply -f $apply_tmp
 kubectl rollout status -f $apply_tmp --timeout=120s
 rm -f $apply_tmp
