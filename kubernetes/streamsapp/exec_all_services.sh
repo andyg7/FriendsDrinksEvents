888c9f49561d22fe9ba41a29f197b54411f8732c
Andrew Grant <andrewgrant243@gmail.com>
2021-06-21 19:00:03 -0400

    Add script to start/stop streams app services

---
+++ kubernetes/streamsapp/exec_all_services.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eu
+
+action=$1
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
+kubectl $action -f apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_invitation_writer_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_meetup_writer_servier.yml andyg001/friendsdrinksbackend:latest apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_request_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_writer_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_request_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_writer_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
+
+rm -rf apply_tmp
