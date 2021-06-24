80ccdd77540bb921fd2bc2dfddbadedb267902f8
Andrew Grant <andrewgrant243@gmail.com>
2021-06-23 21:09:52 -0400

    Use tmp file

--- kubernetes/streamsapp/exec_all_services.sh
+++ kubernetes/streamsapp/exec_all_services.sh
@@ -8,37 +8,37 @@ kubectl $action -f kubernetes/streamsapp/10create_kafka_topics_job.yml
 kubectl $action -f kubernetes/streamsapp/20streams_config.yml
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
-kubectl $action -f apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+kubectl $action -f $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
-kubectl $action -f apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+kubectl $action -f $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_invitation_writer_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
-kubectl $action -f apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_invitation_writer_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+kubectl $action -f $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_meetup_writer_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
-kubectl $action -f apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_meetup_writer_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+kubectl $action -f $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_request_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
-kubectl $action -f apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_request_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+kubectl $action -f $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_writer_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
-kubectl $action -f apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_writer_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+kubectl $action -f $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_request_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
-kubectl $action -f apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_request_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+kubectl $action -f $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_writer_service.yml andyg001/friendsdrinksbackend:latest apply_tmp
-kubectl $action -f apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_writer_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+kubectl $action -f $apply_tmp
 
-rm -rf apply_tmp
+rm -rf $apply_tmp
 
 echo 'Success!'
