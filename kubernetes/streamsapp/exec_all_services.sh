642b4dd62f19fe779cb3dd908f5992737b8c8065
Andrew Grant <andrewgrant243@gmail.com>
2021-06-24 18:51:06 -0400

    Support image name in exec script

--- kubernetes/streamsapp/exec_all_services.sh
+++ kubernetes/streamsapp/exec_all_services.sh
@@ -3,44 +3,50 @@
 set -eu
 
 action=$1
+img=$2
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_invitation_writer_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_invitation_writer_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_meetup_writer_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_meetup_writer_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_request_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_request_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_writer_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_writer_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_request_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_request_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
 apply_tmp=$(mktemp)
-./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_writer_service.yml andyg001/friendsdrinksbackend:latest $apply_tmp
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_writer_service.yml $img $apply_tmp
+kubectl $action -f $apply_tmp
+rm -rf $apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/user_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
