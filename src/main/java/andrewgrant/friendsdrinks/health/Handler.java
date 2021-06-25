4e57d4a9d01c422619b4d017ed3392d83c6090e1
Andrew Grant <andrewgrant243@gmail.com>
2021-06-25 08:10:28 -0400

    Add logging to health checking code

--- src/main/java/andrewgrant/friendsdrinks/health/Handler.java
+++ src/main/java/andrewgrant/friendsdrinks/health/Handler.java
@@ -1,6 +1,8 @@
 package andrewgrant.friendsdrinks.health;
 
 import org.apache.kafka.streams.KafkaStreams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -15,6 +17,8 @@ import com.sun.net.httpserver.HttpHandler;
  */
 public class Handler implements HttpHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(Handler.class);
+
     private KafkaStreams kafkaStreams;
 
     public Handler(KafkaStreams kafkaStreams) {
@@ -25,8 +29,10 @@ public class Handler implements HttpHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         KafkaStreams.State state = kafkaStreams.state();
         if (!state.isRunningOrRebalancing()) {
+            log.warn("Kafka streams is not running or re-balancing {}", state.name());
             throw new RuntimeException(String.format("State is %s", state.name()));
         }
+        log.debug("Kafka streams is healthy");
         String resp = "Success!";
         httpExchange.sendResponseHeaders(200, resp.length());
         OutputStream outputStream = httpExchange.getResponseBody();
