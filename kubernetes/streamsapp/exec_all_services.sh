a54b6641093ab2bc4a26d3dd5e58531249ce1cc2
Andrew Grant <andrewgrant243@gmail.com>
2021-06-25 23:41:47 -0400

    Reduce initial delay

--- kubernetes/streamsapp/exec_all_services.sh
+++ kubernetes/streamsapp/exec_all_services.sh
@@ -10,45 +10,62 @@ apply_tmp=$(mktemp)
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
+sleep 15
+
 apply_tmp=$(mktemp)
 ./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_api_service_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
+
+sleep 15
 
 apply_tmp=$(mktemp)
 ./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_invitation_writer_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
+sleep 15
+
 apply_tmp=$(mktemp)
 ./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_meetup_writer_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
+
+sleep 15
 
 apply_tmp=$(mktemp)
 ./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_request_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
+sleep 15
+
 apply_tmp=$(mktemp)
 ./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_membership_writer_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
+
+sleep 15
 
 apply_tmp=$(mktemp)
 ./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_request_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
+sleep 15
+
 apply_tmp=$(mktemp)
 ./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/friendsdrinks_writer_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
+
+sleep 15
 
 apply_tmp=$(mktemp)
 ./kubernetes/streamsapp/generate_manifest.sh kubernetes/streamsapp/user_service.yml $img $apply_tmp
 kubectl $action -f $apply_tmp
 rm -rf $apply_tmp
 
-echo 'Success!'
+sleep 15
 
+echo 'Success!'
