af1ba40f8f7349a26bd4dce6272873c1239ddf1f
Andrew Grant <andrewgrant243@gmail.com>
2021-06-26 06:42:41 -0400

    Increase heartbeat interval

--- src/main/java/andrewgrant/friendsdrinks/streamsconfig/Config.java
+++ src/main/java/andrewgrant/friendsdrinks/streamsconfig/Config.java
@@ -12,6 +12,7 @@ public class Config {
 
     public static java.util.Properties addSharedConfig(java.util.Properties streamsConfig) {
         streamsConfig.put(StreamsConfig.consumerPrefix(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG), "120000");
+        streamsConfig.put(StreamsConfig.consumerPrefix(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG), "15000");
         if (System.getenv(STREAMS_GROUP_INSTANCE_ID_ENV_VAR) != null) {
             streamsConfig.put(StreamsConfig.consumerPrefix(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG),
                     System.getenv(STREAMS_GROUP_INSTANCE_ID_ENV_VAR));
