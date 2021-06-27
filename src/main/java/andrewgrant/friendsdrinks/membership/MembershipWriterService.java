e69205cb04a81288a86cf3580d25c25fde37a8c2
Andrew Grant <andrewgrant243@gmail.com>
2021-06-26 22:00:05 -0400

    Use new UncaughtExceptionHandler in membership writer service

--- src/main/java/andrewgrant/friendsdrinks/membership/MembershipWriterService.java
+++ src/main/java/andrewgrant/friendsdrinks/membership/MembershipWriterService.java
@@ -4,6 +4,7 @@ import static andrewgrant.friendsdrinks.env.Properties.load;
 
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
 import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.slf4j.Logger;
@@ -103,9 +104,9 @@ public class MembershipWriterService {
         Topology topology = writerService.buildTopology();
         Properties streamProps = writerService.buildStreamsProperties(envProps);
         KafkaStreams kafkaStreams = new KafkaStreams(topology, streamProps);
-        kafkaStreams.setUncaughtExceptionHandler((Thread thread, Throwable throwable) -> {
-            log.error("Uncaught exception {}", throwable.getMessage());
-            throwable.printStackTrace();
+        kafkaStreams.setUncaughtExceptionHandler(exception -> {
+            log.error("Uncaught exception {}", exception.getMessage());
+            return StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.REPLACE_THREAD;
         });
         log.info("Starting MembershipWriterService application...");
 
