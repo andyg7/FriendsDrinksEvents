11b84acd022e851b437649c3a264e6383f990003
Andrew Grant <andrewgrant243@gmail.com>
2021-07-04 15:05:48 -0400

    Remove code that looks for STREAMS_GROUP_INSTANCE_ID_ENV_VAR in Properties.java

--- src/main/java/andrewgrant/friendsdrinks/streamsconfig/Properties.java
+++ src/main/java/andrewgrant/friendsdrinks/streamsconfig/Properties.java
@@ -8,9 +8,6 @@ import java.io.IOException;
  */
 public class Properties {
 
-    public static final String STREAMS_GROUP_INSTANCE_ID = "group.instance.id";
-    private static final String STREAMS_GROUP_INSTANCE_ID_ENV_VAR = "STREAMS_GROUP_INSTANCE_ID";
-
     public static java.util.Properties load(String fileName) throws IOException {
         java.util.Properties envProps = new java.util.Properties();
         FileInputStream input = new FileInputStream(fileName);
@@ -18,9 +15,6 @@ public class Properties {
             envProps.load(input);
         } finally {
             input.close();
-        }
-        if (System.getenv(STREAMS_GROUP_INSTANCE_ID_ENV_VAR) != null) {
-            envProps.setProperty(STREAMS_GROUP_INSTANCE_ID, System.getenv(STREAMS_GROUP_INSTANCE_ID_ENV_VAR));
         }
         return envProps;
     }
