c6c957e7ffa13798b1820d31569eba4d2b1a9ac5
Andrew Grant <andrewgrant243@gmail.com>
2021-07-04 11:58:30 -0400

    Add back StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG config

--- src/main/java/andrewgrant/friendsdrinks/frontend/kafkastreams/MaterializedViewsService.java
+++ src/main/java/andrewgrant/friendsdrinks/frontend/kafkastreams/MaterializedViewsService.java
@@ -573,6 +573,7 @@ public class MaterializedViewsService {
                 envProps.getProperty("bootstrap.servers"));
         streamProps.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
                 envProps.getProperty("schema.registry.url"));
+        streamProps.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         if (envProps.getProperty("streams.dir") != null) {
             streamProps.put(StreamsConfig.STATE_DIR_CONFIG, envProps.getProperty("streams.dir"));
         }
