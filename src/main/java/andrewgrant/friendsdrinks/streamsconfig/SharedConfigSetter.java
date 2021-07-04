895c2494580a5ea090f046d1a9b389d3e425949e
Andrew Grant <andrewgrant243@gmail.com>
2021-07-04 15:17:28 -0400

    Log a warning when env var STREAMS_GROUP_INSTANCE_ID_ENV_VAR isnt set

--- src/main/java/andrewgrant/friendsdrinks/streamsconfig/SharedConfigSetter.java
+++ src/main/java/andrewgrant/friendsdrinks/streamsconfig/SharedConfigSetter.java
@@ -2,11 +2,15 @@ package andrewgrant.friendsdrinks.streamsconfig;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.streams.StreamsConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  */
 public class SharedConfigSetter {
+
+    private static final Logger log = LoggerFactory.getLogger(SharedConfigSetter.class);
 
     private static final String STREAMS_GROUP_INSTANCE_ID_ENV_VAR = "STREAMS_GROUP_INSTANCE_ID";
 
@@ -16,6 +20,8 @@ public class SharedConfigSetter {
         if (System.getenv(STREAMS_GROUP_INSTANCE_ID_ENV_VAR) != null) {
             streamsConfig.put(StreamsConfig.consumerPrefix(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG),
                     System.getenv(STREAMS_GROUP_INSTANCE_ID_ENV_VAR));
+        } else {
+            log.warn("Failed to find {} env var", STREAMS_GROUP_INSTANCE_ID_ENV_VAR);
         }
         return streamsConfig;
     }
